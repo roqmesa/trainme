321aca26858f236316890ae64ddff84c623d6b91
Roque Mesa <roque.mesa@gmail.com>
2017-04-03 14:11:11 -0500

    setup;

---
+++ db/migrate/20170403185753_create_trainers_and_clients.rb
@@ -0,0 +1,25 @@
+class CreateTrainersAndClients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trainers do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null:false
+      t.string :email, null:false
+      t.string :city, null:false
+      t.string :state, null:false
+      t.string :certification, null:false
+      t.text :about, null:false
+      t.string :specialties, null:false
+    end
+
+    create_table :clients do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null:false
+      t.string :email, null:false
+      t.string :city, null:false
+      t.string :state, null:false
+      t.text :about, null:false
+      t.string :specialties, null:false
+    end
+
+  end
+end
